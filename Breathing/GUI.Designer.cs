4edbf3b3f4393428908e9050e3a968b4529d5ed6
Em0ting0fficer <mrosegger1006@gmail.com>
2018-03-09 16:22:46 +0100

    Initial commit

---
+++ Breathing/GUI.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Breathing
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.speedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extraSlowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.slowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fastToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doubleBufferedMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.antiAliasingMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrBreath = new System.Windows.Forms.Timer(this.components);
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speedToolStripMenuItem,
+            this.settingsMenuItem,
+            this.helpMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(284, 24);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // speedToolStripMenuItem
+            // 
+            this.speedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.extraSlowToolStripMenuItem,
+            this.slowToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.fastToolStripMenuItem});
+            this.speedToolStripMenuItem.Name = "speedToolStripMenuItem";
+            this.speedToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.speedToolStripMenuItem.Text = "&Speed";
+            // 
+            // extraSlowToolStripMenuItem
+            // 
+            this.extraSlowToolStripMenuItem.Name = "extraSlowToolStripMenuItem";
+            this.extraSlowToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.extraSlowToolStripMenuItem.Text = "Extra Slow";
+            // 
+            // slowToolStripMenuItem
+            // 
+            this.slowToolStripMenuItem.Name = "slowToolStripMenuItem";
+            this.slowToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.slowToolStripMenuItem.Text = "Slow";
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.normalToolStripMenuItem.Text = "Normal";
+            // 
+            // fastToolStripMenuItem
+            // 
+            this.fastToolStripMenuItem.Name = "fastToolStripMenuItem";
+            this.fastToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.fastToolStripMenuItem.Text = "Fast";
+            // 
+            // settingsMenuItem
+            // 
+            this.settingsMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.doubleBufferedMenuItem,
+            this.antiAliasingMenuItem});
+            this.settingsMenuItem.Name = "settingsMenuItem";
+            this.settingsMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsMenuItem.Text = "S&ettings";
+            // 
+            // doubleBufferedMenuItem
+            // 
+            this.doubleBufferedMenuItem.Name = "doubleBufferedMenuItem";
+            this.doubleBufferedMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.doubleBufferedMenuItem.Text = "&DoubleBuffered";
+            // 
+            // antiAliasingMenuItem
+            // 
+            this.antiAliasingMenuItem.Name = "antiAliasingMenuItem";
+            this.antiAliasingMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.antiAliasingMenuItem.Text = "A&ntiAliasing";
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutMenuItem});
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpMenuItem.Text = "&Help";
+            // 
+            // aboutMenuItem
+            // 
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutMenuItem.Text = "&About";
+            // 
+            // tmrBreath
+            // 
+            this.tmrBreath.Enabled = true;
+            this.tmrBreath.Interval = 25;
+            this.tmrBreath.Tick += new System.EventHandler(this.tmrBreath_Tick);
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "GUI";
+            this.Text = "Breathing";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GUI_Paint);
+            this.Resize += new System.EventHandler(this.GUI_Resize);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem speedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem slowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fastToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doubleBufferedMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem antiAliasingMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extraSlowToolStripMenuItem;
+        private System.Windows.Forms.Timer tmrBreath;
+    }
+}
+
---
+++ Breathing/GUI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Breathing
+ * Ziele:
+ * - Animierte Ellipse
+ * - Graphische Textausgabe
+ * - Menü
+ * - About-Dialog
+ * Max Rosegger
+ * 09.03.2018*/
+
+namespace Breathing
+{
+    public partial class GUI : Form
+    {
+        float size;
+        float height;
+        float width;
+        bool isCircleGrowing = true;
+        const int minSize = 10;
+        float maxSize;       
+
+        public GUI()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            size = 10;
+            height = ClientSize.Height / 2 - size / 2;
+            width = ClientSize.Width / 2 - size / 2;
+            isCircleGrowing = true;
+        }
+
+        private void GUI_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Green);
+            myPen.Width = 10;
+            RectangleF circle = new RectangleF(width, height, size, size);
+            g.DrawEllipse(myPen, circle);
+        }
+
+        private void tmrBreath_Tick(object sender, EventArgs e)
+        {
+            int margin = mainMenu.Height*3;
+            float smallerSide = Math.Min(ClientSize.Width, ClientSize.Height);
+            maxSize = smallerSide - margin;
+            if (size < maxSize && isCircleGrowing)
+            {
+                size+=smallerSide / 100;
+                height-= smallerSide / 100 / 2;
+                width -= smallerSide / 100 / 2;
+            }
+            else if (size >= maxSize)
+            {
+                isCircleGrowing = false;
+            }
+
+            if (size > minSize && !isCircleGrowing)
+            {
+                size-=smallerSide / 100;
+                height += smallerSide / 100 / 2;
+                width += smallerSide / 100 / 2;
+            }
+            else if (size <= minSize)
+            {
+                isCircleGrowing = true;
+            }
+            Invalidate();
+        }
+
+        private void GUI_Resize(object sender, EventArgs e)
+        {
+            height = ClientSize.Height / 2 - size / 2;
+            width = ClientSize.Width / 2 - size / 2;
+            if (size > maxSize)
+            {
+                size = maxSize;
+            }
+            if (size < minSize)
+            {
+                size = minSize;
+            }
+        }
+    }
+}
